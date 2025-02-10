3089f6aaa63f4db6f3572201355695337f1e8ac6
Agustin <torresagustin1603@gmail.com>
2025-02-10 16:38:42 -0300

    Agregando UsuariosComponent usando hooks con fetch

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,11 +1,11 @@
-import { FormularioComponent } from "./components/FormularioComponent"
+import { UsuariosComponent } from "./components/UsuariosComponent"
 
 export const HooksApp = () => {
   return (
     <>
         <h1>Aplicación de Hooks</h1>
         <hr />
-        <FormularioComponent></FormularioComponent>
+        <UsuariosComponent></UsuariosComponent>
     </>
   )
 }
