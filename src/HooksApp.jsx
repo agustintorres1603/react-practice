7d16156b4b5a681c61c4accdea1cf7e97d7bcef2
Agustin <torresagustin1603@gmail.com>
2025-02-12 16:54:34 -0300

    Uso de useReducer

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,11 +1,11 @@
-import { CallbackComponent } from "./components/CallbackComponent"
+import { ListaTareas } from "./components/ListaTareas"
 
 export const HooksApp = () => {
   return (
     <>
         <h1>Aplicación de Hooks</h1>
         <hr />
-        <CallbackComponent></CallbackComponent>
+        <ListaTareas></ListaTareas>
     </>
   )
 }
